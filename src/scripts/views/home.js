0df38f2cda016f61508e42e75d99b43e5846b1a8
rach syahputra <rach.syahputra@gmail.com>
2024-07-13 02:40:06 +0800

    display note-item and note-item-detail and create click event listener for note-item

--- src/scripts/views/home.js
+++ src/scripts/views/home.js
@@ -2,9 +2,32 @@ import Notes from '../data/notesData.js'
 
 const home = () => {
   const noteListContainerElement = document.querySelector('#noteListContainer')
+  const noteDetailContainerElement = document.querySelector('#noteDetailContainer')
   const noteListElement = noteListContainerElement.querySelector('note-list')
 
   const showNoteList = () => {
+    const result = Notes.getAll()
+    const noteItemElements =  result.map((note) => {
+      const noteItemElement = document.createElement('note-item')
+      
+      noteItemElement.note = note
+
+      noteItemElement.addEventListener('click', () => {
+        const noteItemDetailElement = document.querySelector('note-item-detail')
+        noteItemDetailElement.note = note
+        console.log(noteItemDetailElement.note)
+      })
+
+      return noteItemElement
+    })
+    noteListElement.append(...noteItemElements)
+  }
+
+  const onClickNoteItem = () => {
+
+  }
+
+  const showNoteDetail = () => {
     const result = Notes.getAll()
     const noteItemElements =  result.map((note) => {
       const noteItemElement = document.createElement('note-item')
@@ -13,7 +36,7 @@ const home = () => {
       return noteItemElement
     })
 
-    noteListElement.append(...noteItemElements)
+    noteDetailElement.append(...noteItemElements)
   }
 
   showNoteList()
