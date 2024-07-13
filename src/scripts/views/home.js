36a8e929845276663b7409760709fa71877cbe0e
rach syahputra <rach.syahputra@gmail.com>
2024-07-13 13:57:16 +0800

    implement show form add note and show note detail logic when clicking add note button

--- src/scripts/views/home.js
+++ src/scripts/views/home.js
@@ -3,9 +3,12 @@ import Utils from '../Utils.js'
 
 const home = () => {
   const noteListContainerElement = document.querySelector('#noteListContainer')
-  const noteDetailContainerElement = document.querySelector('#noteDetailContainer')
   const noteListElement = noteListContainerElement.querySelector('note-list')
+
+  const noteDetailContainerElement = document.querySelector('#noteDetailContainer')
+  const noteDetailElement = noteDetailContainerElement.querySelector('#noteDetail')
   const noNotesSelectedElement = noteDetailContainerElement.querySelector('no-notes-selected')
+  const buttonAddNoteElement = noteDetailContainerElement.querySelector('button-add-note')
 
   const showNoteList = () => {
     const result = Notes.getAll()
@@ -15,6 +18,17 @@ const home = () => {
       noteItemElement.note = note
 
       noteItemElement.addEventListener('click', () => {
+        const formAddNoteElement = noteDetailContainerElement.querySelector('form-add-note')
+
+        if (formAddNoteElement) {
+          formAddNoteElement.remove()
+
+          buttonAddNoteElement.setAttribute('src', 'src/assets/plus.png')
+          isFormAddNote = false
+
+          Utils.showElement(noteDetailElement, 'flex')
+        }
+
         const noteItemDetailElement = document.querySelector('note-item-detail')
         noteItemDetailElement.note = note
         Utils.hideElement(noNotesSelectedElement)
@@ -25,17 +39,27 @@ const home = () => {
     noteListElement.append(...noteItemElements)
   }
 
-  const showNoteDetail = () => {
-    const result = Notes.getAll()
-    const noteItemElements =  result.map((note) => {
-      const noteItemElement = document.createElement('note-item')
-      noteItemElement.note = note
+  const showFormAddNote = () => {
+    Utils.hideElement(noteDetailElement)
 
-      return noteItemElement
-    })
+    const formAddNoteElement = document.createElement('form-add-note')
+    noteDetailContainerElement.appendChild(formAddNoteElement)
 
-    noteDetailElement.append(...noteItemElements)
+    buttonAddNoteElement.setAttribute('src', 'src/assets/check.png')
   }
+
+  let isFormAddNote = false
+
+  buttonAddNoteElement.addEventListener('click', () => {
+    if (isFormAddNote) {
+      showNoteDetail()
+      isFormAddNote = false
+    } else {
+      showFormAddNote()
+      isFormAddNote = true
+    }
+    
+  }) 
 
   showNoteList()
 }
