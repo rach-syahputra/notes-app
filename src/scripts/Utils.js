59b485c2f2dc9fe63dbb9ebfa135683bb46527b1
rach syahputra <rach.syahputra@gmail.com>
2024-07-13 17:47:06 +0800

    add formatDate util

--- src/scripts/Utils.js
+++ src/scripts/Utils.js
@@ -8,6 +8,20 @@ class Utils {
     element.style.display = 'none'
     element.hidden = true
   }
+
+  static formatDate(dateStr) {
+    const date = new Date(dateStr)
+
+    const day = date.getUTCDate()
+    const month = date.toLocaleString('default', { month: 'long' })
+    const year = date.getUTCFullYear()
+    const hours = String(date.getUTCHours()).padStart(2, '0')
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0')
+
+    const formattedDate = `${day} ${month} ${year}, ${hours}:${minutes}`
+
+    return formattedDate
+  }
 }
 
 export default Utils
