6736ae95b763dc1025be1f02f96265d3c31ffeb8
rach syahputra <rach.syahputra@gmail.com>
2024-07-16 17:17:20 +0800

    feat: add getArchivedNotes method in Notes Api service

--- src/scripts/data/remote/notes-api.js
+++ src/scripts/data/remote/notes-api.js
@@ -18,6 +18,23 @@ class NotesAPi {
     }
   }
 
+  static async getArchivedNotes() {
+    try {
+      const response = await fetch(`${BASE_URL}/notes/archived`)
+
+      if (!response.status >= 200 && response.status < 300) {
+        throw new Error('Something went wrong')
+      }
+
+      const responseJson = await response.json()
+      const { data } = responseJson
+
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   static async getSingleNote(noteId) {
     try {
       const response = await fetch(`${BASE_URL}/notes/${noteId}`)
